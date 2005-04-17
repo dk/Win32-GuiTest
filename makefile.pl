53ea26de3ff32787db977da729946452fadd535f
ctrondlp <ctrondlp>
2005-04-17 06:45:50 +0000

    Added some code to help with environments where file name case is of concern.

--- makefile.pl
+++ makefile.pl
@@ -1,20 +1,29 @@
 #!/usr/bin/perl
 #
-# $Id: makefile.pl,v 1.6 2005/02/08 19:03:08 ctrondlp Exp $
+# $Id: makefile.pl,v 1.7 2005/04/17 06:45:50 ctrondlp Exp $
 #
 
 use ExtUtils::MakeMaker;
+
+# Ensure correct file case for case-sensitive environments
+rename('guitest.pm', 'GuiTest.pm');
+rename('guitest.xs', 'GuiTest.xs');
+rename('manifest', 'MANIFEST');
+rename('manifest.skip', 'MANIFEST.SKIP');
+rename('examples.pm', 'Examples.pm');
+rename('makefile.pl', 'Makefile.PL');
+rename('changes', 'CHANGES');
+
 # See lib/ExtUtils/MakeMaker.pm for details of how to influence
 # the contents of the Makefile that is written.
-
 my @make = (
     'NAME'	=> 'Win32::GuiTest',
-    'VERSION_FROM' => 'guitest.pm', # finds $VERSION
+    'VERSION_FROM' => 'GuiTest.pm', # finds $VERSION
     'LIBS'	=> [''],   # e.g., '-lm' 
     'DEFINE'	=> '',     # e.g., '-DHAVE_SOMETHING' 
     'INC'	=> '',     # e.g., '-I/usr/include/other' 
     'OBJECT'    => 'GuiTest$(OBJ_EXT) DibSect$(OBJ_EXT)'  ,
-    'XS'	=> { 'guitest.xs' => 'guitest.cpp' },
+    'XS'	=> { 'GuiTest.xs' => 'GuiTest.cpp' },
     'TYPEMAPS'  => ['perlobject.map' ],
 #    'XSOPT' => '-c++'
 #    'CCFLAGS' => '-MD -DWIN32 -Z7 -DDEBUG -D_DEBUG',
@@ -24,7 +33,7 @@ my @make = (
 
 # Add additional settings for the creation of PPD files
 if ($ExtUtils::MakeMaker::VERSION >= 5.43) {
-    push @make, 'ABSTRACT_FROM' => 'guitest.pm';
+    push @make, 'ABSTRACT_FROM' => 'GuiTest.pm';
     push @make, 'AUTHOR' => 'Dennis K. Paulsen (ctrondlp@cpan.org), Ernesto Guisado (erngui@acm.org)';
 }
 
